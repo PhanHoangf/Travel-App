bbcfd178916c522ceb1c55737bb39447b1c7c3b6
PhanHoangf <17520519@gm.uit.edu.vn>
2018-12-10 21:53:09 +0700

    Project Midterm

---
+++ TravelPlannerApp/TravelPlannerApp/Travel.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TravelPlannerApp
+{
+    partial class Travel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser1);
+            this.splitContainer1.Size = new System.Drawing.Size(1103, 618);
+            this.splitContainer1.SplitterDistance = 353;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 587);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "69 địa điểm du lịch";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(7, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 548);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(746, 618);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Travel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 618);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Travel";
+            this.Text = "Domestic Travel";
+            this.Load += new System.EventHandler(this.Travel_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ TravelPlannerApp/TravelPlannerApp/Travel.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelPlannerApp
+{
+    public partial class Travel : Form
+    {
+        public Travel()
+        {
+            InitializeComponent();
+        
+        }
+
+      
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Travel_Load(object sender, EventArgs e)
+        {
+
+            Places();
+
+
+        }
+        private void Places()
+        {
+            for (int i = 1; i < 69; i++)
+            {
+                Button btn = new Button();
+
+                if (i == 1)
+                    btn.Text = "Mộc Châu (Sơn La)";
+                if (i == 2)
+                    btn.Text = "Mai Châu ( Hòa Bình)";
+                if (i == 3)
+                    btn.Text = "Tam Đảo (Vĩnh Phúc)";
+                if (i == 4)
+                    btn.Text = "Hàm Lợn (Hà Nội)";
+                if (i == 5)
+                    btn.Text = "Háng Tề Chơ(Yên Bái)";
+                if (i == 6)
+                    btn.Text = "Mù Cang Chải (Yên Bái)";
+                if (i == 7)
+                    btn.Text = "Sapa (Lào Cai)";
+                if (i == 8)
+                    btn.Text = "Hồ Pa Khoang (Điện Biên)";
+                if (i == 9)
+                    btn.Text = "Vườn Quốc gia Xuân Sơn (Phú Thọ)";
+                if (i == 10)
+                    btn.Text = "Đồng Cao (Bắc Giang)";
+                if (i == 11)
+                    btn.Text = "Thác Bản Giốc (Cao Bằng)";
+                if (i == 12)
+                    btn.Text = "Vườn quốc gia Ba Vì (Hà Nội)";
+                if (i == 13)
+                    btn.Text = "Hồ Thung Nai (Hòa Bình)";
+                if (i == 14)
+                    btn.Text = "Vịnh Lan Hạ (Hải Phòng)";
+                if (i == 15)
+                    btn.Text = "Trekking Tây Yên Tử (Quảng Ninh)";
+                if (i == 16)
+                    btn.Text = "Bãi Dài (Quảng Ninh)";
+                if (i == 17)
+                    btn.Text = "Bình Liêu (Quảng Ninh)";
+                if (i == 18)
+                    btn.Text = "Hồ Núi Cốc (Thái Nguyên)";
+                if (i == 19)
+                    btn.Text = "Hồ Quan Sơn (Hà Nội)";
+                if (i == 20)
+                    btn.Text = "Côn Sơn Kiếp Bạc (Hải Dương)";
+                if (i == 21)
+                    btn.Text = "Đỉnh Lảo Thẩn (Lào Cai)";
+                if (i == 22)
+                    btn.Text = "Tà Xùa (Sơn La)";
+                if (i == 23)
+                    btn.Text = "Bạch Mộc Lương Tử (Lào Cai)";
+                if (i == 24)
+                    btn.Text = "Núi Muối (Lào Cai)";
+
+                if (i == 25)
+                    btn.Text = "Háng Đồng (Sơn La)";
+
+                if (i == 26)
+                    btn.Text = "Y Tý (Lào Cai)";
+
+                if (i == 27)
+                    btn.Text = "Sìn Hồ (Lai Châu)";
+
+                if (i == 28)
+                    btn.Text = "Pù Luông (Thanh Hóa)";
+
+                if (i == 29)
+                    btn.Text = "Bà Nà (Đà Nẵng)";
+
+                if (i == 30)
+                    btn.Text = "Đảo Lý Sơn (Quảng Ngãi)";
+
+
+                if (i == 31)
+                    btn.Text = "Cù Lao Xanh (Bình Định)";
+
+                if (i == 32)
+                    btn.Text = "Biển Bình Tiên (Ninh Thuận)";
+
+                if (i == 33)
+                    btn.Text = "Đảo Phú Quý (Bình Thuận)";
+
+                if (i == 34)
+                    btn.Text = "Động Thiên Đường (Quảng Bình)";
+
+                if (i == 35)
+                    btn.Text = "Vườn quốc gia Pù Mát (Nghệ An)";
+                if (i == 36)
+                    btn.Text = "Suối Cá Thần Cẩm Lương (Thanh Hóa)";
+
+                if (i == 37)
+                    btn.Text = "Vực Phun (Phú Yên)";
+
+                if (i == 38)
+                    btn.Text = "Vịnh Vũng Rô (Phú Yên)";
+
+                if (i == 39)
+                    btn.Text = "Bến En (Thanh Hóa)";
+
+                if (i == 40)
+                    btn.Text = "Đến biển Sầm Sơn (Thanh Hóa)";
+                if (i == 41)
+                    btn.Text = "Rừng quốc gia Pù Mát, Con Cuông (Nghệ An)";
+                if (i == 42)
+                    btn.Text = "Biển Thiên Cầm (Hà Tĩnh)";
+                if (i == 43)
+                    btn.Text = "Thung Nham - Thung Chim (Ninh Bình)";
+                if (i == 44)
+                    btn.Text = "Đèo Hải Vân – Đầm Lập An – Vườn Quốc Gia Bạch Mã (Thừa Thiên Huế)";
+                if (i == 45)
+                    btn.Text = "Quốc lộ 49B: 5 đầm, 3 biển, 1 động, 1 phá (Huế)";
+                if (i == 46)
+                    btn.Text = "Đèo A Co – Thác A Nor (Huế)";
+                if (i == 47)
+                    btn.Text = "Hồ Chí Minh - đèo Pêke (Huế)";
+                if (i == 48)
+                    btn.Text = "Khe Tre - Thác Mơ";
+                if (i == 49)
+                    btn.Text = "Biển An Bàng";
+                if (i == 50)
+                    btn.Text = "Cù Lao Chàm";
+                if (i == 51)
+                    btn.Text = "Đà Lạt (Lâm Đồng)";
+                if (i == 52)
+                    btn.Text = "Nam Du (Kiên Giang)";
+                if (i == 53)
+                    btn.Text = "Đường ven biển Ninh Thuận";
+                if (i == 54)
+                    btn.Text = "Bảo Lộc - Đà Lạt - Dran (Đà Lạt)";
+                if (i == 55)
+                    btn.Text = "Bình Ba";
+                if (i == 56)
+                    btn.Text = "Bình Lập";
+                if (i == 57)
+                    btn.Text = "Bình Hưng";
+                if (i == 58)
+                    btn.Text = "Biển Bình Tiên – Nàng công chúa ngủ quên";
+                if (i == 59)
+                    btn.Text = "Phú Quốc (Kiên Giang)";
+                if (i == 60)
+                    btn.Text = "Vũng Tàu";
+                if (i == 61)
+                    btn.Text = "Long An";
+                if (i == 62)
+                    btn.Text = "Tiền Giang";
+                if (i == 63)
+                    btn.Text = "Bến Tre";
+                if (i == 64)
+                    btn.Text = "Trà Vinh";
+                if (i == 65)
+                    btn.Text = "Sóc Trăng";
+                if (i == 66)
+                    btn.Text = "Cần Thơ";
+                if (i == 67)
+                    btn.Text = "Vĩnh Long";
+                if (i == 68)
+                    btn.Text = "Đồng Tháp";
+
+
+
+                btn.AutoSize = true;
+                btn.Location = new Point(10, panel1.Controls.Count * 25);
+                panel1.Controls.Add(btn);
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
